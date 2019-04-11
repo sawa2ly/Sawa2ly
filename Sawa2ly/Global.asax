4798aebf26c54d1f1237ef1fa397ec3d7a2e94bf
AdelRedaa97 <38018487+AdelRedaa97@users.noreply.github.com>
2019-04-11 21:23:37 +0100

    first commit

---
+++ Sawa2ly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sawa2ly.MvcApplication" Language="C#" %>
---
+++ Sawa2ly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sawa2ly.Startup))]
+namespace Sawa2ly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
