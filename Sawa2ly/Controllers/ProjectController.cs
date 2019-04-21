33c72544a04fbda4b7abe6ea96820d94b654f6fb
AdelRedaa97 <38018487+AdelRedaa97@users.noreply.github.com>
2019-04-21 21:59:16 +0200

    project index

--- Sawa2ly/Controllers/ProjectController.cs
+++ Sawa2ly/Controllers/ProjectController.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Sawa2ly.Extensions;
+using Sawa2ly.Models;
+using System.Data.Entity;
 
 namespace Sawa2ly.Controllers
 {
     public class ProjectController : Controller
     {
-        // GET: Project
-        public ActionResult Index()
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Project/1
+        public ActionResult Index(int? id)
         {
-            return View();
+            if (Request.IsAuthenticated) {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Project project = db.Project.Include(c => c.Customer).SingleOrDefault(p => p.Id == id);
+                if (project == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.accessibility = "Customer,MD,MTL,MTS";
+                return View(project);
+            }
+            else
+            {
+                return RedirectToAction("Index", "WelcomeHome");
+            }
         }
     }
 }
