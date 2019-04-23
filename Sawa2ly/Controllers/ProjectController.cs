e2bae377d3e0ac4204b9c7fd69ac046edc729ed0
AdelRedaa97 <38018487+AdelRedaa97@users.noreply.github.com>
2019-04-23 18:05:23 +0200

    project edit module

--- Sawa2ly/Controllers/ProjectController.cs
+++ Sawa2ly/Controllers/ProjectController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Sawa2ly.Extensions;
+using Sawa2ly.ViewModels;
 using Sawa2ly.Models;
 using System.Data.Entity;
 
@@ -21,18 +22,69 @@ namespace Sawa2ly.Controllers
                 {
                     return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 }
-                Project project = db.Project.Include(c => c.Customer).SingleOrDefault(p => p.Id == id);
+                Project project = db.Project
+                    .Include(c => c.Customer)
+                    .Include(c => c.MD)
+                    .Include(c => c.MTL)
+                    .SingleOrDefault(p => p.Id == id);
+
                 if (project == null)
                 {
                     return HttpNotFound();
                 }
-                ViewBag.accessibility = "Customer,MD,MTL,MTS";
-                return View(project);
+                var projectTrainees = db.ProjectTrainees.Where(p => p.ProjectId == project.Id).Include(t => t.MTS).ToList();
+                var userId = User.Identity.GetUserID();
+                if ( userId == project.CustomerId ||  userId == project.MDID || userId == project.MTLID)
+                {
+                    ViewBag.accessibility = "member";
+                }else if ( db.ProjectTrainees.Any(p => p.ProjectId == project.Id && p.MTSID == userId) )
+                {
+                    ViewBag.accessibility = "member";
+                }
+                else
+                {
+                    ViewBag.accessibility = "visitor";
+                }
+                var model = new ProjectAndTrainees { Project = project , ProjectTrainees = projectTrainees};
+                return View(model);
             }
             else
             {
                 return RedirectToAction("Index", "WelcomeHome");
             }
         }
+
+        // GET: Project/EditModule/1
+        public ActionResult EditModule(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Project.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/EditModule/5
+        [HttpPost]
+        public ActionResult EditModule(int proId,DateTime StartDate,DateTime EndDate,int status,Double price)
+        {
+            if (ModelState.IsValid)
+            {
+                var project = db.Project.First(a => a.Id == proId);
+                project.StartDate = StartDate;
+                project.EndDate = EndDate;
+                project.Status = status;
+                project.Price = price;
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = proId });
+            }
+            return View();
+        }
+
     }
 }
---
+++ Sawa2ly/ViewModels/ProjectAndTrainees.cs
@@ -0,0 +1,14 @@
+﻿using Sawa2ly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sawa2ly.ViewModels
+{
+    public class ProjectAndTrainees
+    {
+        public Project Project { get; set; }
+        public List<ProjectTrainees> ProjectTrainees { get; set; }
+    }
+}
